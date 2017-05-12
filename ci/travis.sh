15793c0e9e7c65d0771a57e4f594526fd5995321
Nathaniel J. Smith <njs@pobox.com>
2017-05-12 00:31:48 -0700

    I think it looks prettier this way

--- ci/travis.sh
+++ ci/travis.sh
@@ -7,12 +7,10 @@ pip install -U pip setuptools wheel
 python setup.py sdist --formats=zip
 pip install dist/*.zip
 
-cd docs
-
 # We don't have a test suite, but we can at least make sure that the docs
 # build without warnings. And this also acts as a basic smoke test on the
 # code, since the docs use the code.
-
+cd docs
 # -n (nit-picky): warn on missing references
 # -W: turn warnings into errors
 sphinx-build -nW  -b html source build
